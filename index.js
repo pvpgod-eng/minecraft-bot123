d8e70ef7b2077f1b464e4ec5527c77cd8a3c2136
pvpgod-eng <pvpgod301@gmail.com>
2025-08-06 23:18:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var lastaction;
 var pi = 3.14159;
 var moveinterval = 2; // 2 second movement interval
 var maxrandom = 5; // 0-5 seconds added to movement interval (randomly)
-var host = data["ip"];
+var host = data["pvpgodhypixel123.aternos.me:27863"];
 var username = data["name"]
 var nightskip = data["auto-night-skip"]
 var bot = mineflayer.createBot({
@@ -71,4 +71,5 @@ bot.on('spawn',function() {
 bot.on('death',function() {
     bot.emit("respawn")
 });
+
 
